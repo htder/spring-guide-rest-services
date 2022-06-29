17b76dad40e93dee1b8a77c4c48c72f3723366ed
Henry Derbyshire <henry@htderbyshire.com>
2022-06-29 11:58:31 +0100

    made the /employees get mapping more restful

--- src/main/java/com/watermelon/RESTServices/EmployeeController.java
+++ src/main/java/com/watermelon/RESTServices/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.watermelon.RESTServices;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
@@ -20,8 +22,18 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return this.employeeRepository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+        List<EntityModel<Employee>> employees = this.employeeRepository.findAll().stream()
+                .map(employee -> EntityModel.of(
+                        employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees"))
+                ).collect(Collectors.toList());
+
+        return CollectionModel.of(
+                employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel()
+        );
     }
 
     @PostMapping("/employees")
