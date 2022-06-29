18b9e2d2936b1bb87a9cf894c79bdc5ce371905b
Henry Derbyshire <henry@htderbyshire.com>
2022-06-29 11:50:44 +0100

    updated the get mapping for a single resource more restful

--- src/main/java/com/watermelon/RESTServices/EmployeeController.java
+++ src/main/java/com/watermelon/RESTServices/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.watermelon.RESTServices;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class EmployeeController {
@@ -26,10 +30,12 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return this.employeeRepository.findById(id).orElseThrow(
-                () -> new EmployeeNotFoundException(id)
-        );
+    EntityModel<Employee> one(@PathVariable Long id) {
+        Employee employee = this.employeeRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
     }
 
     @PutMapping("/employees/{id}")
